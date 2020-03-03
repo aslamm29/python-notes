c9e7a24f3f4f1e02943bba752bb3163e690ec52b
aslamm <aslamm.anas29@gmail.com>
2020-03-03 16:15:18 -0500

    Variable conventions

--- variables.py
+++ variables.py
@@ -4,5 +4,16 @@ name = "Aslam"
 location = "ON"
 age, name, location = 28, "Aslam", "ON"
 a = b = c = 1
-userAge = age
-print(age, name, location)
+AGE = 37
+AGE = 41
+userAge = age # Not convention to use camel case
+user_name = "Aslam" # Convention is to use snake case
+__user_location__ = "ON" # Do not change this, up in priority order of variables
+print(AGE)
+
+# Variable Conventions:
+# There are no constants like in JS, every variable can be reassigned
+# The variable that are meant to be constants are in capital letters, please do not reassign
+# The variables are in snake case as a convention as opposed to camel case
+# Variables with double underscore must not be changed, more priority than variables in uppercase, if changed can break something
+# The data types of variables are not required to be mentioned for eg: int, float, string etc. This is called as dynamic typing
