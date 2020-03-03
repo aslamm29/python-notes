78c79451d9564b47e4bebdf61a4b56903759b37d
aslamm <aslamm.anas29@gmail.com>
2020-03-03 16:58:34 -0500

    User inputs and string literals

--- variables.py
+++ variables.py
@@ -21,3 +21,9 @@ print(AGE)
 total_score = None
 # null == None; in python
 print(total_score)
+
+username = input("What's your name?")
+user_location = input ("Where do you live?")
+user_age = input("How old are you?")
+
+print(f"{username} is {user_age} years old and lives in {user_location}")
